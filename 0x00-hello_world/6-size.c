3116c29521c1bceb77f489bb574c36a5638aa928
Lukanya <lukeokanya5@gmail.com>
2022-06-04 11:22:48 +0100

    fix

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main- main contain expression for data sizes
+ * Return: Always 0 if not zero
+ */
+int main(void)
+{
+	printf("size of a char: %d bytre\n@, sizeof(char));
+	printf("size of an int: %d byte(s)\n@,sizeof(int));
+	printf("size of a long int: %d byte(s)\n",sizeof(long int));
+	printf("size of a long long int: % byte(s)\n@, sizeof(long long int));
+	printf(@size of a float: %d byte(s)\n@,sizeof(float));
+	return (0);
+}
